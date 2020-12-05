46cf92b855b6689a80b3f3e54ec6a87c7e797935
taiyou komazawa <taiyou24690@gmail.com>
2020-12-06 03:43:29 +0900

    動作の実装を書いた(動作未検証)

---
+++ src/SerialBridge.h
@@ -0,0 +1,54 @@
+/*
+    SerialBridge.h
+    
+    SerialBridgeの処理をここで定義しています。
+
+    author: Taiyou Komazawa
+*/
+
+#ifndef SERIAL_BRIDGE_H_
+#define SERIAL_BRIDGE_H_
+
+#include <Arduino.h>
+
+#include <Message.h>
+
+class SerialBridge
+{
+public:
+    typedef uint8_t frame_id;
+
+    SerialBridge(HardwareSerial *dev);
+
+    void wait_host(String device_name);
+    void add_msg(_Message *str);
+
+    int read();
+    void write(frame_id id, _Message *str);
+
+protected:
+    enum
+    {
+        FRAME_LEN = 4,
+        STRUCT_MAX_NUM = 10,
+    };
+
+    typedef enum ControlCharType
+    {
+        HEADER = ':',
+        END = '\n',
+    } ctrl_char_t;
+
+private:
+    _Message *_str[STRUCT_MAX_NUM];
+    uint8_t _str_num;
+    uint8_t _max_data_size;
+
+    HardwareSerial *_dev;
+
+    void _wait_host(String device_name);
+    inline void _write_once(uint8_t c, int *data_sum);
+    inline uint8_t _read_once(int *data_sum);
+};
+
+#endif
